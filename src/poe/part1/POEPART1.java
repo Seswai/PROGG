ab35a9a0ef72556c60d666b5516abe1e66c633a9
nkadi <nkadi@KARABOWIZY8>
2024-04-23 19:12:30 +0200

    Second commit

---
+++ src/poe/part1/POEPART1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; 
+public class UserPass {
+    public static void main(String[] args) {
+      
+        Scanner in = new Scanner(system.in);
+         System.out.print("Enter your first name:");
+         String first = in.next();
+         System.out.print("Enter your last name:");
+         String last = in.next();
+         
+        Scanner.class = new Scanner (System.in);
+      
+      String User,Pass;
+      
+      System.out.print("Enter user name");
+      User = item.nextLine();
+      System.out.print("Enter password");
+      Pass = item.nextLine();
+        Object without = null;
+      
+      if(User.equals ("Adam")&&(Pass.equals(without))){
+          System.out.println("welcome back");
+      }else{
+          System.out.println("Wrong User name or password!!!");
+      }
+                  
+      } 
+    
+}
+    
